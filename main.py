3ddf6f47026c7a63eaebad04ffe04551d7286424
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-01-30 23:42:35 +0330

    LED, Buzzer and rgb lamp pin specification

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# main.py -- put your code here!
+from machine import Pin
+
+LED_PIN = 2
+RGB_PIN = 22
+BUZZER_PIN = 5
+
+led = Pin(LED_PIN, Pin.OUT)
+rgb = Pin(RGB_PIN, Pin.OUT)
+buzzer = Pin(BUZZER_PIN, Pin.OUT)
