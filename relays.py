36b8de29967220926313d48832ab2a8792ef5e23
Mohamadali Firoozi <alifiroozi@icloud.com>
2024-02-10 22:00:08 +0330

    Alterd relays.py with Pins class

--- relays.py
+++ relays.py
@@ -1,22 +1,14 @@
 from machine import Pin
+from pins import Pins
 
-RELAY1_PIN = 13
-RELAY2_PIN = 15
-RELAY3_PIN = 14
-RELAY4_PIN = 27
-RELAY5_PIN = 26
-RELAY6_PIN = 25
-RELAY7_PIN = 18
-RELAY8_PIN = 19
-
-relay1 = Pin(RELAY1_PIN, Pin.OUT)
-relay2 = Pin(RELAY2_PIN, Pin.OUT)
-relay3 = Pin(RELAY3_PIN, Pin.OUT)
-relay4 = Pin(RELAY4_PIN, Pin.OUT)
-relay5 = Pin(RELAY5_PIN, Pin.OUT)
-relay6 = Pin(RELAY6_PIN, Pin.OUT)
-relay7 = Pin(RELAY7_PIN, Pin.OUT)
-relay8 = Pin(RELAY8_PIN, Pin.OUT)
+relay1 = Pin(Pins.RELAY1_PIN, Pin.OUT)
+relay2 = Pin(Pins.RELAY2_PIN, Pin.OUT)
+relay3 = Pin(Pins.RELAY3_PIN, Pin.OUT)
+relay4 = Pin(Pins.RELAY4_PIN, Pin.OUT)
+relay5 = Pin(Pins.RELAY5_PIN, Pin.OUT)
+relay6 = Pin(Pins.RELAY6_PIN, Pin.OUT)
+relay7 = Pin(Pins.RELAY7_PIN, Pin.OUT)
+relay8 = Pin(Pins.RELAY8_PIN, Pin.OUT)
 
 realys = [
     relay1,
